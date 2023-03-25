39bdf87f33b6b6fce43b5e974ba9908f452bb58c
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-03-25 17:11:51 +0100

    feat: update page metadata SEO
    - update favicon

--- src/root.tsx
+++ src/root.tsx
@@ -21,8 +21,30 @@ export default function Root() {
         <Title>I don't have spotify</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      // TODO: get favicon
-      // update metdata
+        <Meta name="theme-color" content="#000000" />
+
+        <Meta name="description" content="Convert Spotify links to YouTube, Apple Music, Tidal, Soundcloud and more" />
+        <Meta name="keywords" content="Spotify,Youtube,Apple Music,Tidal,Soundcloud,link converter,search" />
+
+        <Meta property="og:title" content="I don't have Spotify" />
+        <Meta property="og:type" content="website" />
+
+        <Meta property="og:url" content="https://idonthavespotify.sjdonado.de/" />
+        <Meta property="og:site_name" content="I don't have Spotify" />
+        <Meta property="og:description" content="Convert Spotify links to YouTube, Apple Music, Tidal, Soundcloud and more" />
+
+        <Meta property="og:image" content="https://avatars.githubusercontent.com/u/27580836" />
+        <Meta property="og:image:secure_url" content="https://user-images.githubusercontent.com/27580836/227728799-8123c22e-7714-45e7-95b3-56f666d7ebcc.png" />
+        <Meta property="og:image:type" content="image/png" />
+        <Meta property="og:image:alt" content="I don't have Spotify favicon" />
+        {
+          // redis setup (cache searches) -> exp time 1 day
+          // redis counter for searches
+          // add tests e2e
+          // ligthhouse audit
+          // recaptcha
+          // expose api with CORS
+        }
       </Head>
       <Body>
         <Suspense>
