2a8eb8ef7507b340aa049ea059a58d80cc0db26c
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-04-09 11:44:07 +0200

    fix: remove recaptchav3

--- src/config/env/client.ts
+++
@@ -1,9 +0,0 @@
-interface ClientEnv {
-  readonly VITE_RECAPTCHA_SITE_KEY: string;
-}
-
-const { VITE_RECAPTCHA_SITE_KEY } = import.meta.env as ImportMetaEnv & ClientEnv;
-
-export const recaptcha = {
-  siteKey: VITE_RECAPTCHA_SITE_KEY,
-};
