a211192e2c86ce08966ef2b9e61b383c7e69c544
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-10-05 11:55:43 -0500

    feat: error handling + pino loggger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 import { Elysia } from 'elysia';
+import { logger } from '@bogeychan/elysia-logger';
+import pretty from 'pino-pretty';
+
 import { apiRouter } from './routes/api';
 
-const app = new Elysia().use(apiRouter).listen(Bun.env.PORT ?? 3000);
+const stream = pretty({
+  colorize: true,
+});
+
+const app = new Elysia()
+  .use(logger({ stream, level: 'info' }))
+  .on('afterHandle', ctx => {
+    ctx.log.info(ctx.request, 'Request');
+  })
+  .on('error', ({ code, error, log }) => {
+    log.error(error);
+
+    return {
+      code,
+      message: error.message,
+    };
+  })
+  .use(apiRouter)
+  .listen(Bun.env.PORT ?? 3000);
 
 console.log(`Server is running at ${app.server?.hostname}:${app.server?.port}`);
