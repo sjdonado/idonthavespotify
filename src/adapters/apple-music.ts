e78ceb8ad7b4f88bd3e465832eb7ba9080a9a5fe
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-10-05 11:25:35 -0500

    feat: apple-music adapter

---
+++ src/adapters/apple-music.ts
@@ -0,0 +1,66 @@
+import { getCheerioDoc } from '~/utils/scraper';
+import { APPLE_MUSIC_LINK_SELECTOR } from '~/config/constants';
+import { responseMatchesQuery } from '~/utils/compare';
+import { getQueryFromMetadata } from '~/utils/query';
+
+import { SpotifyMetadata, SpotifyMetadataType } from '~/parsers/spotify';
+import { SpotifyContentLink, SpotifyContentLinkType } from '~/services/spotify';
+
+import * as config from '~/config/default';
+
+export async function getAppleMusicLink(
+  metadata: SpotifyMetadata
+): Promise<SpotifyContentLink | undefined> {
+  const query = getQueryFromMetadata(metadata.title, metadata.description, metadata.type);
+
+  const url = `${config.services.appleMusic.baseUrl}${query}`;
+  const html = await fetch(url).then(res => res.text());
+  const doc = getCheerioDoc(html);
+
+  const appleMusicDataByType = {
+    [SpotifyMetadataType.Song]: {
+      href: doc(`div[aria-label="Songs"] ${APPLE_MUSIC_LINK_SELECTOR}`)
+        .first()
+        .attr('href'),
+      title: doc(`div[aria-label="Songs"] ${APPLE_MUSIC_LINK_SELECTOR}`).first().text(),
+      isVerified: true,
+    },
+    [SpotifyMetadataType.Album]: {
+      href: doc(`div[aria-label="Albums"] ${APPLE_MUSIC_LINK_SELECTOR}`)
+        .first()
+        .attr('href'),
+      title: doc(`div[aria-label="Albums"] ${APPLE_MUSIC_LINK_SELECTOR}`).first().text(),
+      isVerified: true,
+    },
+    [SpotifyMetadataType.Playlist]: {
+      href: doc(`div[aria-label="Playlists"] ${APPLE_MUSIC_LINK_SELECTOR}`)
+        .first()
+        .attr('href'),
+      title: doc(`div[aria-label="Playlists"] ${APPLE_MUSIC_LINK_SELECTOR}`)
+        .first()
+        .text(),
+      isVerified: true,
+    },
+    [SpotifyMetadataType.Artist]: {
+      href: doc(`div[aria-label="Artists"] ${APPLE_MUSIC_LINK_SELECTOR}`)
+        .first()
+        .attr('href'),
+      title: doc(`div[aria-label="Artists"] ${APPLE_MUSIC_LINK_SELECTOR}`).first().text(),
+      isVerified: true,
+    },
+    [SpotifyMetadataType.Podcast]: undefined,
+    [SpotifyMetadataType.Show]: undefined,
+  };
+
+  const { title, href, isVerified } = appleMusicDataByType[metadata.type] ?? {};
+
+  if (!responseMatchesQuery(title ?? '', query)) {
+    return undefined;
+  }
+
+  return {
+    type: SpotifyContentLinkType.AppleMusic,
+    url: href ?? url,
+    isVerified,
+  };
+}
