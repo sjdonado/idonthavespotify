6183040c724a35b6c2c7698e7547641df5496d65
Juan Rodriguez <sjdonado@icloud.com>
2024-05-05 19:35:21 +0200

    fix: footer bottom margin

--- src/views/layouts/main.tsx
+++ src/views/layouts/main.tsx
@@ -20,6 +20,7 @@ export default function MainLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
         />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" />
 
         <meta
