33323fab06c8a565d36806a52c386cd2088f2375
Ryan <rsalerno@gmail.com>
2025-12-03 13:13:20 -0700

    fix: searching from Spotify album/song links
    It seems Spotify recently changed how the og:title content is templated, and the additional content in that tag was causing searches to fail.

--- src/parsers/spotify.ts
+++ src/parsers/spotify.ts
@@ -94,21 +94,25 @@ export const getSpotifyMetadata = async (id: string, link: string) => {
 };
 
 export const getSpotifyQueryFromMetadata = (metadata: SearchMetadata) => {
-  // Remove emojis and clean extra whitespace or symbols
   const parsedTitle = metadata.title
+    // Remove suffixes/tags added to the title by Spotify
+    .replace(
+      /(\s-\s(Album|Song)\sby\s(.+?))?\s\|\sSpotify$/g,
+      ''
+    )
+    // Remove emojis
     .replace(
       /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}·]/gu,
       ''
     )
+    // Clean extra whitespace
     .replace(/\s+/g, ' ')
     .trim();
 
   let artist = '';
 
   // Extract the artist from the description based on the metadata type
-  if (metadata.type === MetadataType.Song) {
-    [, artist] = metadata.description.match(/^([^·]+)\s+·/) ?? [];
-  } else if (metadata.type === MetadataType.Album) {
+  if (metadata.type === MetadataType.Song || metadata.type === MetadataType.Album) {
     [, artist] = metadata.description.match(/^([^·]+)\s+·/) ?? [];
   } else if (metadata.type === MetadataType.Podcast) {
     [, artist] = metadata.description.match(/from\s(.+?)\son\sSpotify\./) ?? [];
